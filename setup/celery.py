da4dfeb618c6c8f0c4820d7d40fcff958ec6bc95
Mateus Gurgel <mateusgurgel15@gmail.com>
2024-08-29 20:05:29 -0300

    Refactor: Celery setup moved to setup dir

--- setup/celery.py
+++ setup/celery.py
@@ -1,23 +1,25 @@
 from celery import Celery
 
 from scrapping.task import log_books
+
 from celery.schedules import crontab
 
 from decouple import config
 
 BROKER_URL = config("BROKER_URL", cast=str)
 
-app = Celery('app', broker=BROKER_URL)
+celery_app = Celery('setup', broker=BROKER_URL)
 
-app.conf.beat_schedule = {
-    'add-every-30-seconds': {
-        'task': 'celery_app.collect_data',
+celery_app.conf.beat_schedule = {
+    'Get Book Data': {
+        'task': 'setup.celery.collect_data',
         'schedule': crontab(hour=7, minute=30),
     },
 }
-app.conf.timezone = 'UTC'
 
-@app.task
+celery_app.conf.timezone = 'UTC'
+
+@celery_app.task
 def collect_data():
     print("Collecting data...")
     log_books()
