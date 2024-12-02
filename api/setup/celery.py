090cedb39dc77f93bbb4539b783897d66fb52af9
Mateus Gurgel <mateusgurgel15@gmail.com>
2024-12-02 12:03:27 -0300

    fix: celery config

--- api/setup/celery.py
+++ api/setup/celery.py
@@ -11,13 +11,13 @@ BROKER_URL = config("BROKER_URL", cast=str)
 
 celery_app = Celery('setup', broker=BROKER_URL)
 
-celery_app.conf.timezone = 'UTC'
+celery_app.conf.timezone = 'America/Sao_Paulo'
 celery_app.conf.broker_connection_retry_on_startup = True
 
 celery_app.conf.beat_schedule = {
     'Get Book Data': {
         'task': 'setup.celery.collect_data',
-        'schedule': crontab(hour="12", minute="0"),
+        'schedule': crontab(hour="12", minute="10"),
     },
 }
 
